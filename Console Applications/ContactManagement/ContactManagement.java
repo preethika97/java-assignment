994364ba0d02437ae7e3797ce3bfd56cda5ef9ca
Preethika <112964321+preethika97@users.noreply.github.com>
2022-11-14 22:25:17 +0530

    Create ContactManagement.java

---
+++ Console Applications/ContactManagement/ContactManagement.java
@@ -0,0 +1,9 @@
+package Contactmanagement;
+
+public class ContactManagement {
+
+	public static void main(String[] args) {
+		ContactDetails contactdetails = new ContactDetails();
+		contactdetails.process();
+	}
+}
