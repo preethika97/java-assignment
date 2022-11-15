d28e4a2b4b013453cc731ce38b4add563c465103
Preethika <112964321+preethika97@users.noreply.github.com>
2022-11-15 22:55:49 +0530

    Create StringUpperLower.java

---
+++ StringUpperLower.java
@@ -0,0 +1,10 @@
+
+public class StringUpper {
+
+	public static void main(String[] args) {
+		String a= "preethi";
+		String b="PREETHI";
+		System.out.println("UpperCase : "+a.toUpperCase());
+		System.out.println("LowerCase : "+a.toLowerCase());
+	}
+}
