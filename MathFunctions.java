d96c3e7f9b5527ab9f9e148c412f4b6f8ef58c7d
Preethika <112964321+preethika97@users.noreply.github.com>
2022-11-15 22:58:19 +0530

    Create MathFunctions.java

---
+++ MathFunctions.java
@@ -0,0 +1,18 @@
+
+public class MathFunctions {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Absolute value : " + Math.abs(-45));
+		System.out.println("Round value : " + Math.round(2.288));
+		System.out.println("Ceil value : " + Math.ceil(2.588));
+		System.out.println("Floor value : " + Math.floor(2.588));
+		
+		double a=2;
+		double b=3;
+		System.out.println("Maximum number of a and b : "+Math.max(a,b));
+		System.out.println("Square root of a is : "+Math.sqrt(a));
+		System.out.println("Square root of b is : "+Math.sqrt(b));
+		System.out.println("Power of a and b is : "+Math.pow(a, b));
+	}
+}
